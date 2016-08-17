9c8cc007e00bbf84763d96b43c051b3c99c1791c
Michauxs <363335983@qq.com>
2016-08-17 14:09:07 +0800

    Initial Commit

---
+++ MXSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXSTest
+//
+//  Created by Alfred Yang on 17/8/16.
+//  Copyright © 2016年 Alfred Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MXSTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MXSTest
+//
+//  Created by Alfred Yang on 17/8/16.
+//  Copyright © 2016年 Alfred Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
