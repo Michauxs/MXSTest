8c312eb92e467407141ecb5f71b94f335fa022a7
Michauxs <363335983@qq.com>
2017-11-14 20:20:59 +0800

    todo:nav

---
+++ MXSTest/Common/CommonViews/MXSNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  MXSNavigationBar.h
+//  MXSTest
+//
+//  Created by Alfred Yang on 14/11/17.
+//  Copyright © 2017年 Alfred Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXSNavigationBar : UIView
+
+@end
---
+++ MXSTest/Common/CommonViews/MXSNavigationBar.m
@@ -0,0 +1,21 @@
+//
+//  MXSNavigationBar.m
+//  MXSTest
+//
+//  Created by Alfred Yang on 14/11/17.
+//  Copyright © 2017年 Alfred Yang. All rights reserved.
+//
+
+#import "MXSNavigationBar.h"
+
+@implementation MXSNavigationBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
